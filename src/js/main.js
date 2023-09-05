ddaef6ee2c08ff3c51cd05fe400c4230a0b06aa0
Nick <nick.sieriebriakov@gmail.com>
2023-09-05 23:59:36 +0300

    few fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import { renderMainPage } from "./renderSection/renderMainPage";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { finishedGames } from "./SectionsLogic/finishedGames";
 
 export const quizContainer = document.querySelector(".quiz");
 
